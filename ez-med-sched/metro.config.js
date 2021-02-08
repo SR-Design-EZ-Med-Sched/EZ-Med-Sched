a1b1a5387b8be1e8a614dfb02fb9c3bc7b1c96a1
Keanu Budham <36482340+keanub23@users.noreply.github.com>
2021-02-08 13:32:52 -0500

    Update metro.config.js

--- ez-med-sched/metro.config.js
+++ ez-med-sched/metro.config.js
@@ -2,4 +2,7 @@ module.exports = {
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   },
+  db: {
+		uri: 'mongodb+srv://MedStocks:wktkm4194@cluster0.yhdwm.mongodb.net/test?retryWrites=true&w=majority'
+	}
 };
