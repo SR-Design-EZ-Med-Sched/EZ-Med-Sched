1bbd42a72454337c5447f755991e68e6ae4f0823
wboyd600 <37971619+wboyd600@users.noreply.github.com>
2021-02-26 14:23:52 -0500

    added user profile picture thing

--- ez-med-sched/Views/ProfileScreen/ProfileScreen.js
+++ ez-med-sched/Views/ProfileScreen/ProfileScreen.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-
+import {Text,View,Image, StyleSheet} from 'react-native';
+import default_profile_picture from "../../assets/default_profile_picture.jpg";
 const ProfileScreen = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Profile!</Text>
+    <View style={styles.container}>
+    <Image source={default_profile_picture} style={styles.tinyLogo}></Image>
+    <Text>"BingBong"</Text>
     </View>);
 
 
+
+const styles = StyleSheet.create({
+    container: {
+
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center",
+        height: "100%",
+        textAlign: "center"
+    },
+    tinyLogo: {
+      width: 100,
+      height: 100,
+      borderRadius: 10,
+    },
+    logo: {
+      width: 66,
+      height: 58,
+    },
+  });
 
 export default ProfileScreen;
