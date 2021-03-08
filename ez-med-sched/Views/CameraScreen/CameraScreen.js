4f5ce119d0d4403bf23a3831d688ea0e20896eb1
tysonf <tysonfreeman10@gmail.com>
2021-03-08 15:47:24 -0500

    Captures picture and shows the picture preview

--- ez-med-sched/Views/CameraScreen/CameraScreen.js
+++ ez-med-sched/Views/CameraScreen/CameraScreen.js
@@ -1,11 +1,110 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
-const CameraScreen = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Camera!</Text>
-    </View>);
+let photo;
 
+const CameraScreen = () => {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [previewVisible, setPreviewVisible] = useState(false);
+    const [capturedImage, setCapturedImage] = useState(null);
+    const cameraRef = useRef();
+  
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }, []);
 
+    const takePicture = async () => {
+      if(cameraRef.current)
+      {
+        photo = await cameraRef.current.takePictureAsync();
+        setPreviewVisible(true);
+        setCapturedImage(photo);
+      }
+    }
+  
+    if (hasPermission === null) {
+      return <View />;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
 
-export default CameraScreen;
+    // If picture taken
+    if(previewVisible && capturedImage)
+    {
+      return (
+        <View
+          style={{
+            backgroundColor: 'transparent',
+            flex: 1,
+            width: '100%',
+            height: '100%'
+          }}
+        >
+          <ImageBackground
+            source={{uri: photo && photo.uri}}
+            style={{
+              flex: 1
+            }}
+          />
+        </View>
+      );
+    }
+    // if picture hasn't been taken
+    else
+    {
+
+      return (
+        <Camera
+                style={{flex: 1}}
+                ref = {cameraRef}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    width: '100%',
+                    backgroundColor: 'transparent',
+                    flexDirection: 'row'
+                  }}
+                >
+                  <View
+                    style={{
+                      position: 'absolute',
+                      bottom: 0,
+                      flexDirection: 'row',
+                      flex: 1,
+                      width: '100%',
+                      padding: 20,
+                      justifyContent: 'space-between'
+                    }}
+                  >
+                    <View
+                      style={{
+                        alignSelf: 'center',
+                        flex: 1,
+                        alignItems: 'center'
+                      }}
+                    >
+                      <TouchableOpacity
+                        onPress={takePicture}
+                        style={{
+                          width: 70,
+                          height: 70,
+                          bottom: 0,
+                          borderRadius: 50,
+                          backgroundColor: '#fff'
+                        }}
+                      />
+                    </View>
+                  </View>
+                </View>
+              </Camera>
+      );
+    }
+  }
+  
+ export default CameraScreen;
